310a0515c47631ea665980ff4be5d7145a9b1ef1
Pavlo Grydzhuk <grydzhukpavlo@gmail.com>
2019-01-29 12:38:33 +0200

    added description to select options

--- src/components/DayForm.vue
+++ src/components/DayForm.vue
@@ -27,38 +27,76 @@
         </div>
         <el-form-item label="Indicator">
             <el-select v-model="day.observation.indicator" placeholder="Please select an option">
-                <el-option label="N/A" value=""></el-option>
-                <el-option label="0" value="0"></el-option>
-                <el-option label="2" value="2"></el-option>
-                <el-option label="2W" value="2W"></el-option>
-                <el-option label="4" value="4"></el-option>
-                <el-option label="6" value="6"></el-option>
-                <el-option label="8" value="8"></el-option>
-                <el-option label="10" value="10"></el-option>
-                <el-option label="10DL" value="10DL"></el-option>
-                <el-option label="10SL" value="10SL"></el-option>
-                <el-option label="10WL" value="10WL"></el-option>
+                <el-option label="N/A" value=""><strong>N/A</strong></el-option>
+                <el-option value="0">
+                    <strong>0</strong> - <small>dry</small>
+                </el-option>
+                <el-option value="2">
+                    <strong>2</strong> - <small>damp without lubrication</small>
+                </el-option>
+                <el-option value="2W">
+                    <strong>2W</strong> - <small>wet without lubrication</small>
+                </el-option>
+                <el-option value="4">
+                    <strong>4</strong> - <small>shiny without lubrication</small>
+                </el-option>
+                <el-option value="6">
+                    <strong>6</strong> - <small>sticky</small>
+                </el-option>
+                <el-option value="8">
+                    <strong>8</strong> - <small>tacky</small>
+                </el-option>
+                <el-option value="10">
+                    <strong>10</strong> - <small>stretchy</small>
+                </el-option>
+                <el-option value="10DL">
+                    <strong>10DL</strong> - <small>damp with lubrication</small>
+                </el-option>
+                <el-option value="10SL">
+                    <strong>10SL</strong> - <small>shiny with lubrication</small>
+                </el-option>
+                <el-option value="10WL">
+                    <strong>10WL</strong> - <small>wet with lubrication</small>
+                </el-option>
             </el-select>
         </el-form-item>
         <el-form-item label="Color">
             <el-select v-model="day.observation.color" placeholder="Please select an option">
-                <el-option label="N/A" value=""></el-option>
-                <el-option label="B" value="B"></el-option>
-                <el-option label="C" value="C"></el-option>
-                <el-option label="CK" value="CK"></el-option>
-                <el-option label="K" value="K"></el-option>
-                <el-option label="Y" value="Y"></el-option>
-                <el-option label="G" value="G"></el-option>
-                <el-option label="P" value="P"></el-option>
+                <el-option label="N/A" value="">
+                    <strong>N/A</strong>
+                </el-option>
+                <el-option value="B">
+                    <strong>B</strong> - <small> brown or black spotting</small>
+                </el-option>
+                <el-option value="C">
+                    <strong>C</strong> - <small> cloudy white</small>
+                </el-option>
+                <el-option value="CK">
+                    <strong>CK</strong> - <small> cloudy/clear</small>
+                </el-option>
+                <el-option value="K">
+                    <strong>K</strong> - <small> clear</small>
+                </el-option>
+                <el-option value="Y">
+                    <strong>Y</strong> - <small> yellow</small>
+                </el-option>
             </el-select>
         </el-form-item>
         <el-form-item label="Sensation">
-            <el-radio-group v-model="day.observation.sensation" class="day-form__sensation">
-                <el-radio label="">N/A</el-radio>
-                <el-radio label="L"></el-radio>
-                <el-radio label="G"></el-radio>
-                <el-radio label="P"></el-radio>
-            </el-radio-group>
+            <el-select v-model="day.observation.sensation" placeholder="Please select an option">
+                <el-option label="N/A" value="">
+                    <strong>N/A</strong>
+                </el-option>
+                <el-option value="L">
+                    <strong>L</strong> - <small> lubricate</small>
+                </el-option>
+                <el-option value="G">
+                    <strong>G</strong> - <small> gluey/gummy</small>
+                </el-option>
+                <el-option value="P">
+                    <strong>P</strong> - <small> pasty</small>
+                </el-option>
+            </el-select>
         </el-form-item>
         <el-form-item label="Frequency">
             <el-select v-model="day.observation.frequency" placeholder="Please select an option">
@@ -66,7 +104,9 @@
                 <el-option label="X1" value="X1"></el-option>
                 <el-option label="X2" value="X2"></el-option>
                 <el-option label="X3" value="X3"></el-option>
-                <el-option label="AD" value="AD"></el-option>
+                <el-option value="AD">
+                    <strong>AD</strong> - <small> all day</small>
+                </el-option>
             </el-select>
         </el-form-item>
         <el-row class="show-more">
@@ -77,12 +117,24 @@
                 <div v-show="showMore">
                     <el-form-item label="Menstrual">
                         <el-select v-model="day.observation.menstrual" placeholder="Please select an option">
-                            <el-option label="N/A" value=""></el-option>
-                            <el-option label="H" value="H"></el-option>
-                            <el-option label="M" value="M"></el-option>
-                            <el-option label="L" value="L"></el-option>
-                            <el-option label="VL" value="VL"></el-option>
-                            <el-option label="B" value="B"></el-option>
+                            <el-option value="">
+                                <strong>N/A</strong>
+                            </el-option>
+                            <el-option value="H">
+                                <strong>H</strong> - <small> heavy</small>
+                            </el-option>
+                            <el-option label="M" value="M">
+                                <strong>M</strong> - <small> moderate</small>
+                            </el-option>
+                            <el-option label="L" value="L">
+                                <strong>L</strong> - <small> light</small>
+                            </el-option>
+                            <el-option label="VL" value="VL">
+                                <strong>VL</strong> - <small> very light</small>
+                            </el-option>
+                            <el-option label="B" value="B">
+                                <strong>B</strong> - <small> brown</small>
+                            </el-option>
                         </el-select>
                     </el-form-item>
                     <el-row :gutter="20">
@@ -239,11 +291,11 @@ export default {
 		height: 40px;
     }
     
-    &__sensation {
-        & .el-radio + .el-radio {
-            margin-left: 15px; 
-        }
-    }
+    // &__sensation {
+    //     & .el-radio + .el-radio {
+    //         margin-left: 15px; 
+    //     }
+    // }
 }
 
 .marks {
